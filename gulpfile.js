6727c9c908dde198fcfc510f0b85e838c2414a6e
Mark Savage <mark@savagebits.com>
2016-02-21 21:11:54 -0600

    fixing tabs

--- gulpfile.js
+++ gulpfile.js
@@ -7,51 +7,51 @@ var lint = require('gulp-eslint');
 var ngAnnotate = require('gulp-ng-annotate');
 
 var config = {
-    port: 9008,
-    devBaseUrl: 'http://localhost',
-    paths: {
-        html: [
-            './index.html'
-        ],
-        js: [
-            './app/app.routes.js',
-            './app/app.config.js',
-            '!./app/src/**/*.module.js',
-            './app/src/**/*.js'
-        ],
-        appRoutesJs: './app/app.routes.js',
-        appConfigJs: './app/app.config.js',
-        modulesJs: [
-            './app/app.module.js',
-            './app/src/**/*.module.js'
-        ],
-        bundle: './bundle.js',
-        moduleBundle: './module-bundle.js'
-    },
-    browser: 'chrome'
+  port: 9008,
+  devBaseUrl: 'http://localhost',
+  paths: {
+      html: [
+          './index.html'
+      ],
+      js: [
+          './app/app.routes.js',
+          './app/app.config.js',
+          '!./app/src/**/*.module.js',
+          './app/src/**/*.js'
+      ],
+      appRoutesJs: './app/app.routes.js',
+      appConfigJs: './app/app.config.js',
+      modulesJs: [
+          './app/app.module.js',
+          './app/src/**/*.module.js'
+      ],
+      bundle: './bundle.js',
+      moduleBundle: './module-bundle.js'
+  },
+  browser: 'chrome'
 };
 
 gulp.task('connect', function() {
-    connect.server({
-        root: ['.'],
-        port: config.port,
-        base: config.devBaseUrl,
-        fallback: 'index.html',
-        livereload: true
-    });
+  connect.server({
+      root: ['.'],
+      port: config.port,
+      base: config.devBaseUrl,
+      fallback: 'index.html',
+      livereload: true
+  });
 });
 
 gulp.task('open', ['connect'], function() {
-    return gulp.src('')
-        .pipe(open({
-            app: config.browser,
-            uri: config.devBaseUrl + ':' + config.port + '/'
-        }));
+  return gulp.src('')
+      .pipe(open({
+          app: config.browser,
+          uri: config.devBaseUrl + ':' + config.port + '/'
+      }));
 });
 
 gulp.task('html', function() {
-    return gulp.src(config.paths.html)
-        .pipe(connect.reload());
+  return gulp.src(config.paths.html)
+    .pipe(connect.reload());
 });
 
 gulp.task('js', ['init'], function() {
@@ -66,14 +66,14 @@ gulp.task('js', ['init'], function() {
 });
 
 gulp.task('init', function() {
-    gulp.src(config.paths.modulesJs)
-      .pipe(babel({
-          presets: ['es2015']
-      }))
-      .pipe(ngAnnotate())
-      .pipe(concat(config.paths.moduleBundle))
-      .pipe(gulp.dest('.'))
-      .pipe(connect.reload());
+  gulp.src(config.paths.modulesJs)
+    .pipe(babel({
+        presets: ['es2015']
+    }))
+    .pipe(ngAnnotate())
+    .pipe(concat(config.paths.moduleBundle))
+    .pipe(gulp.dest('.'))
+    .pipe(connect.reload());
 });
 
 gulp.task('lint', function() {
@@ -85,9 +85,9 @@ gulp.task('lint', function() {
 });
 
 gulp.task('watch', function() {
-    gulp.watch(config.paths.html, ['html']);
-    gulp.watch(config.paths.js, ['js', 'lint']);
-    gulp.watch(config.paths.modulesJs, ['js']);
+  gulp.watch(config.paths.html, ['html']);
+  gulp.watch(config.paths.js, ['js', 'lint']);
+  gulp.watch(config.paths.modulesJs, ['js']);
 });
 
 gulp.task('default', ['open', 'js', 'lint', 'watch']);
